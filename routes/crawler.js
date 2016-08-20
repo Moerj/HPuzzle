22f4a4f0afb1726bdf03fa6ee7a69380192d28b0
Moer <199525807@qq.com>
2016-08-20 17:13:36 +0800

    可以随机爬取网络图片了

--- routes/crawler.js
+++ routes/crawler.js
@@ -1,6 +1,8 @@
 var http = require('http')
 var https = require('https')
 var cheerio = require('cheerio')
+var request = require('request')
+var fs = require('fs');
 var host = 'https://yande.re'
 var url = host + '/post?tags=order%3Arandom'; //zealer首页
 
@@ -9,6 +11,7 @@ function GetRandomNum(Min, Max) {
     var Rand = Math.random();
     return (Min + Math.round(Rand * Range));
 }
+
 
 module.exports = function(app) {
     app.get('/yande.re', function(crawler_req, crawler_res) {
@@ -32,8 +35,8 @@ module.exports = function(app) {
                     console.log('所有图片链接：\n', data);
                     resolve(data);
                 })
-            }).on('error', function() {
-                console.log('抓取数据失败！')
+            }).on('error', function(e) {
+                throw new Error(e);
             })
 
         }).then(function(data) {
@@ -45,21 +48,41 @@ module.exports = function(app) {
         }).then(function(url) {
             // 对抽取的数据进一步处理，进入该数据的单独连接中，去除原图的 url
             console.log('正在处理数据...\n', url);
-            https.get(url, function(res) {
-                var html = '';
-                res.on('data', function(data) {
-                    html += data;
+
+            return new Promise(function(resolve, reject) {
+                https.get(url, function(res) {
+                    var html = '';
+                    res.on('data', function(data) {
+                        html += data;
+                    })
+                    res.on('end', function() {
+                        var $ = cheerio.load(html);
+                        var img = $('#image');
+                        var imgurl = img.attr('src');
+                        console.log('postImg完毕！\n', imgurl)
+                        resolve(imgurl);
+                    })
+                }).on('error', function(e) {
+                    throw new Error(e);
                 })
-                res.on('end', function() {
-                    var $ = cheerio.load(html);
-                    var img = $('#image');
-                    var src = img.attr('src');
-                    console.log('postImg完毕！\n', src)
-                    crawler_res.send(src);
-                })
-            }).on('error', function() {
-                console.log('抓取数据失败！')
             })
+
+        }).then(function(url) {
+            console.log('准备下载图片：\n', url);
+            // var req = request(url).pipe(fs.createWriteStream('./dist/images/tempimg.jpg'));
+
+            request(url, function(error, response, body) {
+                if (!error && response.statusCode == 200) {
+                    request.head(url, function(err, res, body) {
+                        request(url).pipe(fs.createWriteStream('./dist/images/tempimg.jpg'))
+                    });
+                }
+            });
+
+            console.log('下载完成，已发送至前台页面。');
+            crawler_res.send({ status: 'ok', src: 'images/tempimg.jpg' });
+            // crawler_res.end();
+
         })
 
 
