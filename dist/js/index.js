6fc7a8d5c64e5204432993bbbcf3c56522a2d7e6
Moer <199525807@qq.com>
2016-08-21 20:01:56 +0800

    UI部分更新

--- dist/js/index.js
+++ dist/js/index.js
@@ -1,43 +1,54 @@
-$(function() {
-    FastClick.attach(document.body);
+'use strict';
 
+FastClick.attach(document.body);
+
+$(function () {
+    // 配置拼图
     var puzzle = Puzzle({
-            imgUrl: 'images/img1.jpg',
-            contanier: '#contanier',
-            size: $('body').width(),
-            level: 1, //设置游戏难度，整数，必须大于1
-            clickSound: 'sounds/slide.wav', //拼图音效
-            clearSound: 'sounds/clear.wav' //胜利音效
-        })
-        .init()
+        imgUrl: 'images/img1.jpg',
+        contanier: '#contanier',
+        size: $('body').width(),
+        level: 1, //设置游戏难度，整数，必须大于1
+        clickSound: 'sounds/slide.wav', //拼图音效
+        clearSound: 'sounds/clear.wav' //胜利音效
+    }).init();
 
-    $('#setLevel').on('change', function() {
-        puzzle.setLevel($(this).val())
-    })
-
-    var t
-    $(window).resize(function() {
-        clearTimeout(t)
-        t = setTimeout(function() {
-            puzzle.reSize($('body').width())
-        }, 300)
+    // 设置等级
+    $('#setLevel').on('change', function () {
+        puzzle.setLevel($(this).val());
     });
 
+    // 窗口改变重置尺寸
+    var t = void 0;
+    $(window).resize(function () {
+        clearTimeout(t);
+        t = setTimeout(function () {
+            puzzle.reSize($('body').width());
+        }, 300);
+    });
+
+    // 创建蒙层
+    var mask = $('<div class="mask">\n        <div class="tost">正在从某网站拔污图,请耐心等待...</div>\n        </div>').appendTo('body').hide();
+
     // 随机一张图片
-    $('#simple').click(function() {
+    $('#simple').click(function () {
         console.log('正在随机加载图片...');
+        mask.show();
+        $('body').css({ overflow: 'hidden' });
+
         // 爬虫
         $.ajax({
             type: "GET",
             url: "/yande.re"
-        }).done(function(res) {
-            var dir = 'images/temp/'
-            var imgUrl = dir + res
+        }).done(function (res) {
+            var dir = 'images/temp/';
+            var imgUrl = dir + res;
             console.log('yande.re爬虫数据：\n', imgUrl);
-            $('#simple').attr('src', imgUrl)
-            puzzle.replaceImg(imgUrl)
-        })
-    })
-
-
-})
+            $('#simple').attr('src', imgUrl);
+            puzzle.replaceImg(imgUrl);
+        }).always(function () {
+            mask.hide();
+            $('body').css({ overflow: '' });
+        });
+    });
+});
---
+++ src/js/index.js
@@ -0,0 +1,60 @@
+FastClick.attach(document.body);
+
+$(function() {
+    // 配置拼图
+    let puzzle = Puzzle({
+            imgUrl: 'images/img1.jpg',
+            contanier: '#contanier',
+            size: $('body').width(),
+            level: 1, //设置游戏难度，整数，必须大于1
+            clickSound: 'sounds/slide.wav', //拼图音效
+            clearSound: 'sounds/clear.wav' //胜利音效
+        })
+        .init()
+
+    // 设置等级
+    $('#setLevel').on('change', function() {
+        puzzle.setLevel($(this).val())
+    })
+
+    // 窗口改变重置尺寸
+    let t
+    $(window).resize(function() {
+        clearTimeout(t)
+        t = setTimeout(function() {
+            puzzle.reSize($('body').width())
+        }, 300)
+    });
+
+    // 创建蒙层
+    let mask = $(`<div class="mask">
+        <div class="tost">正在从某网站拔污图,请耐心等待...</div>
+        </div>`)
+        .appendTo('body').hide()
+
+
+    // 随机一张图片
+    $('#simple').click(function() {
+        console.log('正在随机加载图片...');
+        mask.show()
+        $('body').css({overflow: 'hidden'})
+
+        // 爬虫
+        $.ajax({
+            type: "GET",
+            url: "/yande.re",
+            // timeout: 15000
+        }).done(function(res) {
+            let dir = 'images/temp/'
+            let imgUrl = dir + res
+            console.log('yande.re爬虫数据：\n', imgUrl);
+            $('#simple').attr('src', imgUrl)
+            puzzle.replaceImg(imgUrl)
+        }).always(function() {
+            mask.hide();
+            $('body').css({overflow: ''})
+        })
+    })
+
+
+})
